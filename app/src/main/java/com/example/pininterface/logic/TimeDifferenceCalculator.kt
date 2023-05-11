cf082f55d2b97b15653ff76854d93dead4ce2188
Matthias Teuber <m.teuber94@gmail.com>
2023-05-11 14:19:49 +0200

    added comments for TimeDifferenceCalculator.kt

--- app/src/main/java/com/example/pininterface/logic/TimeDifferenceCalculator.kt
+++ app/src/main/java/com/example/pininterface/logic/TimeDifferenceCalculator.kt
@@ -1,16 +1,31 @@
 package com.example.pininterface.logic
 
+/**
+ * TimeDifferenceCalculator
+ * Class for calculating the TimeDifference
+ */
 class TimeDifferenceCalculator {
+
     private var oldTime = System.currentTimeMillis()
 
+    /**
+     * reset the old time to the current time
+     */
     fun resetTime() {
+
         this.oldTime = System.currentTimeMillis()
     }
 
+    /**
+     * Calcuate the time difference between the old time and the current time.
+     * Once finished it will set the old time as the current time.
+     * @return the time difference as Int
+     */
     fun calcTimeDif(): Int {
-        val newTime = System.currentTimeMillis()
-        val timeDifference = newTime - this.oldTime
-        this.oldTime = newTime
+
+        val currentTime = System.currentTimeMillis()
+        val timeDifference = currentTime - this.oldTime
+        this.oldTime = currentTime
         return timeDifference.toInt()
     }
 }
